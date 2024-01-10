e0735032603d0b973e5368462e2a066e28c50211
Joe <jakatsa5@gmail.com>
2024-01-10 15:12:11 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+
+// .slice() returns the value of the index
+function returnFirstTwoDrivers(drivers) {
+  return drivers.slice(0, 2);
+}
+const returnLastTwoDrivers = (drivers) => drivers.slice(-2);
+// Array containing the two functions as its elements
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+const createFareMultiplier = (number) => {
+  return function (num) {
+    return number * num;
+  };
+};
+const quadruple = createFareMultiplier(4);
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+console.log(double(5));
+console.log(triple(5));
+// Function to select different drivers based on the provided function
+function selectDifferentDrivers(drivers, selectingFunction) {
+  return selectingFunction(drivers);
+}
